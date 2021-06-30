4511f9881c9dbd5104844ff5dfdc35df7a0b328b
Hagsten <andreas.hagsten@gmail.com>
2021-06-30 12:42:30 +0200

    Add project files.

---
+++ Blogg.SemaphoreSlim/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Blogg.SemaphoreSlim
+{
+    class Program
+    {
+        private static readonly string[] Inputs = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        
+        static void Main(string[] args)
+        {
+            //Make sure to have the web app "Blogg.WebApp" running before starting this application.
+            HammerTheApi();
+
+            Console.ReadLine();
+        }
+
+        public static void HammerTheApi()
+        {
+            var rnd = new Random();
+
+            var client = new HttpClient();
+            
+            var semaphore = new System.Threading.SemaphoreSlim(100);
+
+            Parallel.ForEach(Enumerable.Range(0, 10000), async (_,_,_) =>
+            {
+                await semaphore.WaitAsync();
+
+                var input = Inputs[rnd.Next(0, 6)];
+
+                var response = await client.GetStringAsync($"https://localhost:44360/reverse?text={input}");
+
+                Console.WriteLine($"{input} --> {response}");
+
+                semaphore.Release();
+            });
+        }
+
+        public static void HammerTheApiUnControlled()
+        {
+            //The web app "Blogg.WebApp" has a limit of 100 concurrent calls, this code will therefore not work since it is not throttled.
+            var rnd = new Random();
+
+            var client = new HttpClient();
+
+            Parallel.ForEach(Enumerable.Range(0, 10000), async (_, _, _) =>
+            {
+                var input = Inputs[rnd.Next(0, 6)];
+
+                var response = await client.GetStringAsync($"https://localhost:44360/reverse?text={input}");
+
+                Console.WriteLine($"{input} --> {response}");
+            });
+        }
+    }
+}
---
+++ Blogg.WebApp/Controllers/ReverseController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogg.WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReverseController : ControllerBase
+    {
+        public static int ConcurrentCalls = 0;
+
+        [HttpGet]
+        public async Task<string> Get(string text)
+        {
+            if (ConcurrentCalls > 100)
+            {
+                throw new HttpRequestException("Too many calls", null, HttpStatusCode.TooManyRequests);
+            }
+
+            Interlocked.Increment(ref ConcurrentCalls);
+
+            var rnd = new Random();
+
+            await Task.Delay(rnd.Next(50, 5000));
+
+            var response = new string(text.ToCharArray().Reverse().ToArray()) + " " + ConcurrentCalls;
+
+            Interlocked.Decrement(ref ConcurrentCalls);
+
+            return response;
+        }
+    }
+}
